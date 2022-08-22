1b03bf1c0dc0db88057805ceb95fddbfc8c04d03
dxia2p <71045728+dxia2p@users.noreply.github.com>
2022-08-21 19:47:05 -0600

    Update server.js

--- ChatApp/server/server.js
+++ ChatApp/server/server.js
@@ -2,8 +2,8 @@ const { Server } = require("socket.io");
 
 const io = new Server(process.env.PORT || 3000, { 
     cors:{
-        origin: ['https://classy-arithmetic-766bdc.netlify.app/'],
-        methods: ["GET", "POST"],
+        //origin: ['https://classy-arithmetic-766bdc.netlify.app/'],
+        origin: "*",
     }
  });
 
